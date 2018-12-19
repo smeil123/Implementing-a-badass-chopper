e3cd6685c5825bbd4a2430aa9f6a7072d6e7f236
smeil123 <egoing@gmail.com>
2018-12-19 14:35:05 +0900

    지형은 만들었다..

--- src/proj2.js
+++ src/proj2.js
@@ -1,5 +1,5 @@
 "use strict";
-var SRC_VERT = 
+var SRC_VERT =
 'attribute vec4	aPosition;\n'+
 'uniform mat4	MVP;\n'+
 'uniform mat4	MV;\n'+
@@ -7,7 +7,7 @@ var SRC_VERT =
 '	gl_Position = MVP*aPosition;\n'+
 '}\n'
 
-var SRC_FRAG = 
+var SRC_FRAG =
 '#ifdef GL_ES\n'+
 'precision mediump float;\n'+
 '#endif\n'+
@@ -16,7 +16,7 @@ var SRC_FRAG =
 '	gl_FragColor = vec4(uColor, 1);\n'+
 '}\n'
 
-var SRC_VERT_TEX = 
+var SRC_VERT_TEX =
 'attribute vec4	aPosition;\n'+
 'attribute vec2	aTexCoord;\n'+
 'uniform mat4	MVP;\n'+
@@ -27,7 +27,7 @@ var SRC_VERT_TEX =
 '	vTexCoord = aTexCoord;\n'+
 '}\n'
 
-var SRC_FRAG_TEX = 
+var SRC_FRAG_TEX =
 '#ifdef GL_ES\n'+
 'precision mediump float;\n'+
 '#endif\n'+
@@ -37,6 +37,17 @@ var SRC_FRAG_TEX =
 'void main(){\n'+
 '	gl_FragColor = texture2D(uSampler, vTexCoord);\n'+
 '}\n'
+
+// var SRC_VERT_GRID =
+// 'attribute vec2	aPosition;\n'+
+// 'attribute vec2	aTexCoord;\n'+
+// 'uniform mat4	MVP;\n'+
+// 'uniform mat4	MV;\n'+
+// 'varying vec2	vTexCoord;\n'+
+// 'void main(){\n'+
+// '	gl_Position = MVP*vec4(aPosition,0,1);\n'+
+// '	vTexCoord = aTexCoord;\n'+
+// '}\n'
 
 
 var ANGLE_STEP_ROTOR = 60.0;
@@ -55,11 +66,14 @@ function main()
 
 	let fov = 50;
 	let P = new Matrix4();
-	P.setPerspective(50, 1, 1, 100); 
+	P.setPerspective(50, 1, 1, 100);
 
 	let axes = new Axes(gl);
 
 	let shader = new Shader(gl, SRC_VERT, SRC_FRAG, ["aPosition"]);
+	let shader_grid = new Shader(gl, document.getElementById("SRC_VERT_GRID").text,
+	document.getElementById("SRC_FRAG_GRID").text, ["aPosition", "aTexCoord"]);
+
 	let shader_tex = new Shader(gl, SRC_VERT_TEX, SRC_FRAG_TEX, ["aPosition", "aTexCoord"]);
 
 	let body = create_body(gl);
@@ -106,7 +120,7 @@ function main()
 		}
 	};
 
-	let tex = gl.createTexture(); 
+	let tex = gl.createTexture();
 	gl.bindTexture(gl.TEXTURE_2D, tex);
 	gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 	gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
@@ -119,7 +133,8 @@ function main()
 		tex_loaded = true;
 	};
 
-	img.src = 'grasslight-big.jpg';
+	// img.src = 'yorkville.jpg';
+	img.src = 'sky.jpg';
 	let tex_loaded = false;
 
 	let textures = [];
@@ -136,6 +151,7 @@ function main()
 		}
 	};
 
+	let ball = create_mesh_sphere(gl,50);
 
 	let t_last = Date.now();
 
@@ -164,12 +180,14 @@ function main()
 		gl.uniform3f(gl.getUniformLocation(shader.h_prog, "uColor"), .5, 1, .5);
 		rotor.render(gl, shader, null, null, V, P);
 
+		gl.useProgram(shader_grid.h_prog);
+		gl.uniform3f(gl.getUniformLocation(shader_grid.h_prog, "uColor"), .5, 1, .5);
+		ball.render(gl, shader_grid, null, null, V, P,textures);
 
+		// gl.useProgram(shader.h_prog);
+		// terrain.render(gl, shader_tex, null, null, V, P,textures);
 
-		gl.useProgram(shader.h_prog);
-		terrain.render(gl, shader_tex, null, null, V, P,textures);
-
-		requestAnimationFrame(tick, canvas); // Request that the browser calls tick
+		// requestAnimationFrame(tick, canvas); // Request that the browser calls tick
 
 
 	};
@@ -225,47 +243,47 @@ function create_body(gl) {
 		 .1, .1, .1,    1, 0, 0,  // v0 White
 		 .1,-.1, .1,    1, 0, 0,  // v3 Yellow
 		 .1,-.1,-.1,    1, 0, 0,  // v4 Green
-		
+
 		 .1, .1, .1,    1, 0, 0,  // v0 White
 		 .1,-.1,-.1,    1, 0, 0,  // v4 Green
 		 .1, .1,-.1,    1, 0, 0,  // v5 Cyan
-		
+
 		 .1, .1, .1,    0, 1, 0,  // v0 White
 		 .1, .1,-.1,    0, 1, 0,  // v5 Cyan
 		-.1, .1,-.1,    0, 1, 0,  // v6 Blue
-		
+
 		 .1, .1, .1,    0, 1, 0,  // v0 White
 		-.1, .1,-.1,    0, 1, 0,  // v6 Blue
 		-.1, .1, .1,    0, 1, 0,  // v1 Magenta
-		
+
 		 .1, .1, .1,    0, 0, 1,  // v0 White
 		-.1, .1, .1,    0, 0, 1,  // v1 Magenta
 		-.1,-.1, .1,    0, 0, 1,  // v2 Red
-		
+
 		 .1, .1, .1,    0, 0, 1,  // v0 White
 		-.1,-.1, .1,    0, 0, 1,  // v2 Red
 		 .1,-.1, .1,    0, 0, 1,  // v3 Yellow
-		
+
 		-.1,-.1,-.1,   -1, 0, 0,  // v7 Black
 		-.1,-.1, .1,   -1, 0, 0,  // v2 Red
 		-.1, .1, .1,   -1, 0, 0,  // v1 Magenta
-		
+
 		-.1,-.1,-.1,   -1, 0, 0,  // v7 Black
 		-.1, .1, .1,   -1, 0, 0,  // v1 Magenta
 		-.1, .1,-.1,   -1, 0, 0,  // v6 Blue
-		
+
 		-.1,-.1,-.1,    0, 0,-1,  // v7 Black
 		-.1, .1,-.1,    0, 0,-1,  // v6 Blue
 		 .1, .1,-.1,    0, 0,-1,  // v5 Cyan
-		
+
 		-.1,-.1,-.1,    0, 0,-1,  // v7 Black
 		 .1, .1,-.1,    0, 0,-1,  // v5 Cyan
 		 .1,-.1,-.1,    0, 0,-1,  // v4 Green
-		
+
 		-.1,-.1,-.1,    0,-1, 0,  // v7 Black
 		 .1,-.1,-.1,    0,-1, 0,  // v4 Green
 		 .1,-.1, .1,    0,-1, 0,  // v3 Yellow
-		
+
 		-.1,-.1,-.1,    0,-1, 0,  // v7 Black
 		 .1,-.1, .1,    0,-1, 0,  // v3 Yellow
 		-.1,-.1, .1,    0,-1, 0,  // v2 Red
@@ -331,6 +349,74 @@ function create_body(gl) {
 	return new Mesh(gl, "drawArrays", gl.TRIANGLES, 72, attribs, -1, null);
 }
 
+function create_mesh_sphere(gl, SPHERE_DIV)
+{ // Create a sphere
+	var i, ai, si, ci;
+	var j, aj, sj, cj;
+	var p1, p2;
+
+	var positions = [];
+	var indices = [];
+
+	// Generate coordinates
+	for (j = 0; j <= SPHERE_DIV; j++)
+	{
+		aj = j  / SPHERE_DIV;
+		sj = aj * 4 - (4/2);
+		cj = Math.cos(aj);
+		for (i = 0; i <= SPHERE_DIV; i++)
+		{
+			ai = i  / SPHERE_DIV;
+			si = ai * 4 - (4/2);
+			ci = Math.cos(ai);
+
+			positions.push(sj);  // X
+			positions.push(si);       // Y
+			// positions.push(0);  // Z
+		}
+	}
+
+	// Generate indices
+	for (j = 0; j < SPHERE_DIV; j++)
+	{
+		for (i = 0; i < SPHERE_DIV; i++)
+		{
+			p1 = j * (SPHERE_DIV+1) + i;
+			p2 = p1 + (SPHERE_DIV+1);
+
+			indices.push(p1);
+			indices.push(p2);
+			indices.push(p1 + 1);
+
+			indices.push(p1 + 1);
+			indices.push(p2);
+			indices.push(p2 + 1);
+		}
+	}
+
+	var buf_position = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buf_position);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+	var buf_normal = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, buf_normal);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+	var attribs = [];
+	var FSIZE = positions.BYTES_PER_ELEMENT;
+//
+	attribs["aPosition"] = {buffer:buf_position, size:2, type:gl.FLOAT, normalized:false, stride:0, offset:0};
+	attribs["aNormal"] = {buffer:buf_normal, size:3, type:gl.FLOAT, normalized:false, stride:0, offset:0};
+	attribs["aTexCoord"] = {buffer:buf_position, size:2, type:gl.FLOAT, normalized:false, stride:0, offset:0};
+
+	var buf_index = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, buf_index);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+	return new Mesh(gl, "drawElements", gl.TRIANGLES, indices.length, attribs, buf_index, gl.UNSIGNED_SHORT);
+}
 
 
 function create_terrain(gl)
@@ -353,5 +439,3 @@ function create_terrain(gl)
 	return new Mesh(gl, "drawArrays", gl.TRIANGLE_FAN, verts.length / 5, attribs, -1, null);
 
 }
-
-
